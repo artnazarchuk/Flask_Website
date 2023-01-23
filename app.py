de869e8af003d1023ca89ba700c67c40c7be40e6
Artiom Nazarchuk <sel_it@tut.by>
2023-01-23 16:25:35 +0300

    Add templates home and about

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return 'About page'
+    return render_template('about.html')
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
