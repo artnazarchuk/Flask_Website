b23401c365fff1a745ca522c504b0c3eed7dcd14
Artjom Nazarchuk <sel_it@tut.by>
2023-01-29 19:40:44 +0300

    Adding articles to the site

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,11 +23,25 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    articles = Article.query.order_by(Article.date).all()
+    return render_template('about.html', articles=articles)
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'User page ' + name + ' - ' + str(id)
+@app.route('/article', methods=['POST', 'GET'])
+def article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article_create = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article_create)
+            db.session.commit()
+            return redirect('/about')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+    else:
+        return render_template('article.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
